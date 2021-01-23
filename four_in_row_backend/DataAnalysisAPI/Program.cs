546eb88aa45d797535e2fa9931e56510fa697769
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-01-23 18:44:13 +0100

    [FIR-1] Configurazione backend <-> frontend - code cleaning

--- four_in_row_backend/DataAnalysisAPI/Program.cs
+++ four_in_row_backend/DataAnalysisAPI/Program.cs
@@ -54,16 +54,4 @@ namespace FourInRow
                 })
             .UseNLog();
     }
-
-    /*public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }*/
 }
--- four_in_row_backend/DataAnalysisAPI/Startup.cs
+++ four_in_row_backend/DataAnalysisAPI/Startup.cs
@@ -14,32 +14,6 @@ using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace FourInRow
 {
-    /*public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSignalR();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseFileServer();
-
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChatHub>("/chat");
-            });
-        }
-    }*/
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
