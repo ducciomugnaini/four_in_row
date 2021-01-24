9025eb8e64d8c4cfb798f3005b26da0823f4436a
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-01-24 19:47:48 +0100

    [FIR-7] Implementazione logiche automatiche di gioco - creazione della struttura del progetto

---
+++ four_in_row_backend/FourInRowAI_Simple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FourInRowAI_Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
