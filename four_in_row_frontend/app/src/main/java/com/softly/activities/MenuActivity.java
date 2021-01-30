86c1ef56b3bd568dbcffc6125065b26dd893d860
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-01-30 02:35:02 +0100

    [FIR-5] Android - Reimplementare le utility per la comunicazione con il server

--- four_in_row_frontend/app/src/main/java/com/softly/activities/MenuActivity.java
+++ four_in_row_frontend/app/src/main/java/com/softly/activities/MenuActivity.java
@@ -1,11 +1,21 @@
 package com.softly.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.softly.R;
+
+import java.net.URL;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -19,5 +29,51 @@ public class MenuActivity extends AppCompatActivity {
             Intent intent = new Intent(MenuActivity.this, ChatActivity.class);
             startActivity(intent);
         });
+
+        Button restButton = findViewById(R.id.btn_callRest);
+        restButton.setOnClickListener(v -> {
+            /*AsyncTaskRunner runner = new AsyncTaskRunner();
+            runner.execute();*/
+
+            final TextView textView = (TextView) findViewById(R.id.txt_result);
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(MenuActivity.this);
+            String url = "https://jsonplaceholder.typicode.com/posts";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            textView.setText("Response is: " + response.substring(0, 500));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("That didn't work!");
+                }
+            });
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        });
     }
+
+    private class AsyncTaskRunner extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... params) {
+            return "ciao";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textView = findViewById(R.id.txt_result);
+            textView.setText(result);
+        }
+    }
+
+
 }
