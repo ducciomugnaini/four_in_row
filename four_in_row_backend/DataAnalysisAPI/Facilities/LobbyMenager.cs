a27e0c23e82813aa0fe8d3f641d687061774fa7f
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-04-04 12:00:31 +0200

    [FIR-28] Gestire la coda di gioco con le lobby attive

--- four_in_row_backend/DataAnalysisAPI/Facilities/LobbyMenager.cs
+++ four_in_row_backend/DataAnalysisAPI/Facilities/LobbyMenager.cs
@@ -5,6 +5,13 @@ using System.Threading.Tasks;
 
 namespace FourInRow.Facilities
 {
+    /*
+    https://www.dotnetperls.com/lazy
+    https://docs.microsoft.com/en-us/dotnet/api/system.lazy-1?view=net-5.0#constructors
+    Constructors. In an overloaded constructor, you can specify thread safety, 
+    and even specify a Func type that serves as a factory design pattern method.
+    */
+
     public sealed class LobbyManager
     {
         private static readonly Lazy<LobbyManager> lazy = new Lazy<LobbyManager>(() => new LobbyManager());
